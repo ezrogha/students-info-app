de8807c5da9a6c53edb9f29ba9222ec74e561d97
ezrogha <rtimbiti@gmail.com>
2021-09-11 00:04:38 +0300

    trigger redeployment

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from '@material-ui/styles';
-import { theme } from './theme';
+import { theme } from './theme'; 
 
 ReactDOM.render(
   <React.StrictMode>
